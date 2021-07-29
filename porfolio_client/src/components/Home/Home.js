ca28f807dca119b3c6b12d08481ca35e07184011
julileon10 <julio.leon.diaz1@gmail.com>
2021-07-29 14:41:46 -0400

    responsiveness test

--- porfolio_client/src/components/Home/Home.js
+++ porfolio_client/src/components/Home/Home.js
@@ -12,6 +12,7 @@ export default function Projects() {
 
     const snakeGamePath = './assets/Projects/snake-game.png'
     const leagueFinderPath = './assets/Projects/league-finder.png'
+    const bookAppPath = './assets/Projects/book-app.png'
     
     return (
         <div className='home'>
@@ -50,7 +51,16 @@ export default function Projects() {
                     people I am going against!</p>
                 </div>
                 <div className="project-image-container flex-container">
-                    <img src={leagueFinderPath} width='70%' data-aos='fade-left' alt='' />
+                    <img src={leagueFinderPath} width='75%' data-aos='fade-left' alt='' />
+                </div>
+            </div>
+
+            <div className='project-container flex-container'>
+                <div className="project-image-container flex-container">
+                    <img src={bookAppPath} width='75%' data-aos='fade-right' alt='' />
+                </div>
+                <div className='project-description flex-container' data-aos='fade-left'>
+                    <p>A book app that lets you add books that you've read, or books that you want to read. Also lets you search through a huge library of books. Pretty much any book you can think of will be there. And you can store books, and come back to check the books added so that you can keep track of the books you want to rea, or already read.</p>
                 </div>
             </div>
 
