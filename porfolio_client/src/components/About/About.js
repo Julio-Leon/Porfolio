53e6289c123e97256864b98767322d331bd239ab
Julio leon <julioleon@julios-mbp.mynetworksettings.com>
2023-08-21 10:37:38 -0400

    upated about section

--- porfolio_client/src/components/About/About.js
+++ porfolio_client/src/components/About/About.js
@@ -10,7 +10,7 @@ export default function About() {
             <h1 className='about-title'>
                 Who Am I?
             </h1>
-            <p className='about'>Welcome to my Portfolio, My name is Julio Leon. I am a young software developer based on New York that is very passionate about programming. I love solving really hard puzzles and algorithms because of the satisfaction that comes after I get it done. But what I love the most is that feeling I get when something I create is useful to someone out there, that motivates me more and more. This is who I am, and I would love to know who you are my email is 'julio.leon.diaz1@gmail.com', feel free to contact me!</p>
+            <p className='about'>Welcome to my Portfolio, My name is Julio Leon. I am a young software developer based on New York with a Bachelor's Degree in Computer Science. What I love the most is that feeling I get when something I create is useful to someone out there, that motivates me more. This is who I am, and I would love to know who you are my email is 'julio.leon.diaz1@gmail.com', feel free to contact me!</p>
         </div>
     )
 }
